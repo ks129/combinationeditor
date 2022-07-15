d4ff0594a3dd710c5fe1c8800341493965c7d664
Karlis Suvi <45097959+ks129@users.noreply.github.com>
2022-07-15 18:30:10 +0300

    Add newline after license in index.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 *  @copyright 2022 Karlis Suvi
 *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 */
+
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 
